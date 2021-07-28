7d5617c91a4961c3e0e4cbe71df5c4a264e6c435
Marcelo Júnior <marcelohjw@gmail.com>
2021-07-28 08:18:17 -0300

    Inline styles learned

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput 
+          placeholder="Digite aqui" 
+          style={{borderColor: 'gray', borderWidth: 1, width: '80%', padding: 5}}
+        />
+        <Button title="Add"></Button>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
